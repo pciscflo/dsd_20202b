05431c0c93b690d85cac3f14d3bc10d468fcac64
escorpio80@gmx.com <escorpio80@gmx.com>
2020-10-24 22:09:43 -0500

    24.10.12

--- democrud/src/main/java/com/upc/crudproductos/democrud/facade/ProductoFacadeImpl.java
+++ democrud/src/main/java/com/upc/crudproductos/democrud/facade/ProductoFacadeImpl.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api") //DAO REST
 public class ProductoFacadeImpl implements ProductoFacade{
 
     @Autowired
--- democrud/src/main/java/com/upc/crudproductos/democrud/servicios/ServicioProducto.java
+++ democrud/src/main/java/com/upc/crudproductos/democrud/servicios/ServicioProducto.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface ServicioProducto {
     List<Producto> obtenerProductos();
     Producto obtenerEntidad(Long codigo);
-    Producto crearProducto(Producto producto);
+    Producto crearProducto(Producto producto); //.T.O == Value Object
     Producto actualizarProducto(Producto producto);
     Producto borrarProducto(Long codigo);
 }
